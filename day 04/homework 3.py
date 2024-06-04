6ae22ecca1c6a17ae69b781299ad7be2af99d4cd
chikovani123 <lazarechikovani545@gmail.com>
2024-06-05 00:03:42 +0300

    commit

---
+++ day 04/homework 3.py
@@ -0,0 +1,19 @@
+print(5 +5)
+print(113 + 1234)
+print(60 + 120)
+print(5 + 100)
+
+print(100 - 40)
+print(239-39)
+print(900 - 834)
+print(1000 - 500)
+
+print(5 * 4)
+print(20 * 11)
+print(40 * 40)
+print(120 * 0)
+
+print(500 / 100)
+print(1000 / 250)
+print(700 / 5)
+print(800 / 4)
