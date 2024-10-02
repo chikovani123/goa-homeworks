bc9367be094945255943ca7f457545aaa9b20ce3
chikovani123 <lazarechikovani545@gmail.com>
2024-10-02 19:58:03 +0300

    commit

---
+++ day 20/classwork.py
@@ -0,0 +1,49 @@
+#classwork 1
+num1 = input("enter any number you want:")
+num2 = input("again enter any number you want: ")
+
+result = num1 == num2
+
+print(result)
+
+#classwork 2
+age1 = input("enter your age:")
+age2 = input("enter your age:")
+
+result = age1 > age2
+
+print(result)
+
+#classwork 3
+
+num1 = int (input("enter any number you want: "))
+num2 = int (input("enter any number you want: "))
+num3 = int (input("enter any number you want: "))
+
+result = num1 > 0 and num2 > 0 and num3 > 0
+
+print(result)
+
+#classwork 4
+num = int(input("enter any number you want: "))
+
+result = num >= 0
+
+print(result)
+
+#classwokr 5
+answer = input('enter only yes or no: ')
+
+result = answer == "yes"
+
+print(result)
+
+#classwork 6
+
+num2 = int (input("enter any number you want: "))
+num2 = int (input("enter any number you want: "))
+num1 = int(input("enter any number you want: "))
+
+result = num1 < num2 and num1 < num3
+
+print(result)
